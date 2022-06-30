b7e2d6004d035492ad56204d38d756b104308c15
yjlee <yjlee@dfocus.net>
2022-06-30 10:38:59 +0900

    stream을 활용한 list 값 변경 Test

--- src/main/java/nextstep/fp/StreamStudy.java
+++ src/main/java/nextstep/fp/StreamStudy.java
@@ -5,6 +5,7 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -16,18 +17,28 @@ public class StreamStudy {
         List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
 
         long count = 0;
-        for (String w : words) {
-            if (w.length() > 12) count++;
-        }
+//        for (String w : words) {
+//            if (w.length() > 12) count++;
+//        }
+        count = words.stream().filter(word -> word.length() > 12 ).count();
         return count;
     }
 
-    public static void printLongestWordTop100() throws IOException {
+    public static List<String> printLongestWordTop100() throws IOException {
         String contents = new String(Files.readAllBytes(Paths
                 .get("src/main/resources/fp/war-and-peace.txt")), StandardCharsets.UTF_8);
         List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
 
         // TODO 이 부분에 구현한다.
+        List<String> longestSortedWordsTop100 = words.stream()
+                .distinct()
+                .filter(word -> word.length() > 12)
+                .sorted((a,b) -> {return b.length() - a.length();})
+                .limit(100)
+                .map(word -> word.toLowerCase())
+                .collect(Collectors.toList());
+
+        return longestSortedWordsTop100;
     }
 
     public static List<Integer> doubleNumbers(List<Integer> numbers) {
@@ -39,6 +50,11 @@ public class StreamStudy {
     }
 
     public static long sumOverThreeAndDouble(List<Integer> numbers) {
-        return 0;
+        long result = numbers.stream()
+                .filter(number -> number > 3)
+                .map(number -> number * 2)
+                .reduce(0, (x, y) -> x + y);
+
+        return result;
     }
 }
